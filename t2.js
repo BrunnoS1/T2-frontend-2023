660b53c71174a7d49a9ac1021599c605cfc009bd
BrunnoS1 <129430164+BrunnoS1@users.noreply.github.com>
2023-11-16 13:07:49 -0300

    commit

---
+++ t2.js
@@ -0,0 +1,56 @@
+const readline = require("readline");
+
+// pra ler inputs no console
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const axios = require("axios");
+
+require('dotenv').config();
+// usar .env pra deixar a minha chave api fora do codigo
+
+const units = "metric";
+// unidade de medida
+const lang = "pt_BR";
+// idioma da resposta
+const limit = "1";
+// limitar a 1 cidade na busca
+
+// appid = "ef0b0973b783e0614ac87612ec04344b"       // comentado pois foi feito com .env para teste
+
+
+rl.question('Digite uma cidade: ', function(q) {
+    console.log(`cidade = ${q}`);
+    // buscar latitude e longitude da cidade
+    const url_latlon = `http://api.openweathermap.org/geo/1.0/direct?q=${q}&limit=${limit}&appid=${process.env.appid}`
+
+    axios
+        .get(url_latlon)
+        .then((res) =>{
+            // separar a parte de interesse
+            const data = res.data[0];
+            // console.log(data);
+            // pegar latitude e longitude
+            const lat = data.lat;
+            const lon = data.lon;
+            // buscar o clima da latitude/longitude
+            const url_clima = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&appid=${process.env.appid}&lang=${lang}`
+
+            axios
+            .get(url_clima)
+            .then((res)=>{
+                // console.log(res.data)
+                return res.data
+            })
+            .then((res) => {
+                // pegar sensaçao termica e descrição do clima na resposta e exibir
+                const sens_term = res.main.feels_like
+                const descricao = res.weather[0].description
+                console.log(`Sensaçao térmica = ${sens_term}°C\nDescrição = ${descricao}`)
+            })
+            return data;
+        })
+    rl.close();
+});
